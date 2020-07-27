26f5228ae0cabdd696cf34a723261dfea5ee723e
Sari Sabban <sari.sabban@gmail.com>
2020-07-27 16:04:35 +0300

    Update DeepClass_PointNet.py

--- DeepClass_PointNet.py
+++ DeepClass_PointNet.py
@@ -6,7 +6,6 @@ import tensorflow as tf
 from numpy import array
 from numpy import argmax
 from sklearn import utils
-#import dask.dataframe as dd
 from keras import Input, Model
 from keras.optimizers import Adam
 from keras.models import Sequential
@@ -19,7 +18,6 @@ warnings.warn = warn
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 data = pd.read_csv('DeepClass.csv')
-#data = dd.read_csv('DeepClass.csv')
 data = data.fillna(0)
 data = utils.shuffle(data)
 ''' X features '''
