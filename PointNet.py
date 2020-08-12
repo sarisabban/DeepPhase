77a9e44977f342d9e4ac3b298408f4ffa41c3fa9
Sari Sabban <sari.sabban@gmail.com>
2020-08-12 21:36:29 +0300

    Update and rename DeepClass_PointNet.py to PointNet.py

--- PointNet.py
+++ PointNet.py
@@ -18,19 +18,11 @@ def warn(*args, **kwargs): pass
 warnings.warn = warn
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-with h5py.File('Y.hdf5', 'r') as Yh:
-	Y = Yh['default'][()]
-with h5py.File('Space.hdf5', 'r') as Sh:
-	Space = Sh['default'][()]
-with h5py.File('UnitC.hdf5', 'r') as Uh:
-	UnitC = Uh['default'][()]
-with h5py.File('Coord.hdf5', 'r') as Ch:
-	Coord = Ch['default'][()]
+with h5py.File('Y.hdf5', 'r') as Yh: Y = Yh['default'][()]
+with h5py.File('Coord.hdf5', 'r') as Ch: Coord = Ch['default'][()]
 # Split train/tests/valid
-Xs_train, Xs_tests, Xu_train, Xu_tests, Xc_train, Xc_tests, Y_train, Y_tests =\
-	train_test_split(Space, UnitC, Coord, Y, test_size=0.20)
-Xs_train, Xs_valid, Xu_train, Xu_valid, Xc_train, Xc_valid, Y_train, Y_valid =\
-	train_test_split(Xs_train, Xu_train, Xc_train, Y_train, test_size=0.25)
+X_train, X_tests, Y_train, Y_tests = train_test_split(Coord, Y, test_size=0.20)
+X_train, X_valid, Y_train, Y_valid = train_test_split(X_train, Y_train, test_size=0.25)
 ''' Neural Network '''
 shapeC = Coord.shape[1:]
 n_feat = shapeC[0]
@@ -89,4 +81,4 @@ c = Dense(n_clas, activation='softmax')(c)
 c = Flatten()(c)
 modelC = Model(inputs=input_points, outputs=c)
 modelC.compile(optimizer=Adam(lr=0.001, decay=0.7), loss='categorical_crossentropy', metrics=['accuracy'])
-modelC.fit(Xc_train, Y_train, validation_data=(Xc_valid, Y_valid), epochs=200, batch_size=32)
+modelC.fit(X_train, Y_train, validation_data=(X_valid, Y_valid), epochs=200, batch_size=32)
