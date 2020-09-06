fe693ee0e9ce1d64c5fbafcb566cdd319fc25da8
Sari Sabban <sari.sabban@gmail.com>
2020-09-06 19:36:32 +0300

    Update Crystal.py

--- Crystal.py
+++ Crystal.py
@@ -1,18 +1,17 @@
 import os
 import sys
-import h5py
 import tqdm
-import random
 import Bio.PDB
-import numpy as np
-import urllib.request
 from iotbx.reflection_file_reader import any_reflection_file
+import h5py
+import random
+import numpy as np
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 class ClassData():
 	'''
 	Build a dataset for protein classification (Alpha/Not_Alpha)
-	from x-ray diffraction and get top 10,000 F-Obs reflections only
+	from x-ray diffractions and get the top 10,000 E-value reflections only
 	'''
 	def download(self, ID):
 		''' Downloads a structure's .mtz and .pdb files '''
@@ -41,9 +40,12 @@ class ClassData():
 				ms_all = ms_base.complete_set()
 				ms=ms_all.customized_copy(space_group_info=a.space_group_info())
 				S = str(ms).split()[-1].split(')')[0]
-				# F-obs
-				ff = list(a.expand_to_p1().f_sq_as_f().data())
-				# Convert miller hkl to polar
+				# E-values in P1 space group
+				a.setup_binner(auto_binning=True)
+				a.binner()
+				e_val = a.quasi_normalize_structure_factors()
+				ee = list(e_val.expand_to_p1().f_sq_as_f().data())
+				# Convert miller hkl to polar P1 space group
 				polar_coordinates = list(C.reciprocal_space_vector(P1))
 				xx = []
 				yy = []
@@ -55,20 +57,20 @@ class ClassData():
 				C = str(C)[1:-1]
 				C = tuple(map(str, C.split(', ')))
 				NC = []
-				for x, y, z, r, f in zip(xx, yy, zz, rr, ff):
+				for x, y, z, r, e in zip(xx, yy, zz, rr, ee):
 					if r >= 10.0 or r <= 2.5: continue
-					else: NC.append((x, y, z, r, f))
-				# Sort and choose top max_size F-Obs
+					else: NC.append((x, y, z, r, e))
+				# Sort and choose according to top E-values
 				SORTED = sorted(NC, reverse=True, key=lambda c:c[4])
 				SORTED = SORTED[:size]
-				X, Y, Z, R, F = [], [], [], [], []
+				X, Y, Z, R, E = [], [], [], [], []
 				for i in SORTED:
 					X.append(i[0])
 					Y.append(i[1])
 					Z.append(i[2])
 					R.append(i[3])
-					F.append(i[4])
-				return(S, C, X, Y, Z, R, F)
+					E.append(i[4])
+				return(S, C, X, Y, Z, R, E)
 	def labels(self, filename):
 		structure = Bio.PDB.PDBParser(QUIET=True).get_structure('X', filename)
 		dssp = Bio.PDB.DSSP(structure[0], filename, acc_array='Wilke')
@@ -89,7 +91,7 @@ class ClassData():
 		with open('DeepClass.csv', 'a') as TheFile:
 			header = ['PDB_ID,Label,Space_Group,Unit-Cell_a,Unit-Cell_b,Unit-Cell_c,Unit-Cell_Alpha,Unit-Cell_Beta,Unit-Cell_Gamma']
 			for i in range(1, max_size+1):
-				header.append(',X_{},Y_{},Z_{},Resolution_{},F-obs_{}'\
+				header.append(',X_{},Y_{},Z_{},Resolution_{},E-value_{}'\
 				.format(i, i, i, i, i))
 			header = ''.join(header)
 			TheFile.write(header + '\n')
@@ -100,19 +102,19 @@ class ClassData():
 					try:
 						self.download(item)
 					except:
-						print('\u001b[31m[-] {} failed to download\u001b[0m'\
+						print('\u001b[31m[-] {} Failed to download\u001b[0m'\
 						.format(item.upper()))
 						continue
 					try:
-						Mfilename = item + '.mtz'
-						Pfilename = item + '.pdb'
-						S,C,X,Y,Z,R,F = self.features(Mfilename, size=max_size)
+						Mfilename = './Class/' + item + '.mtz'
+						Pfilename = './Class/' + item + '.pdb'
+						S,C,X,Y,Z,R,E = self.features(Mfilename, size=max_size)
 						H_frac, S_frac, L_frac = self.labels(Pfilename)
 						if H_frac >= 0.5 or S_frac == 0.0:
 							label = 'Alpha'
 						else:
 							label = 'Not_Alpha'
-						assert len(X) == len(Y) == len(Z) == len(R) == len(F),\
+						assert len(X) == len(Y) == len(Z) == len(R) == len(E),\
 						'\u001b[31m[-] {} Failed: values not equal\u001b[0m'.\
 						format(item.upper())
 						exp = [S]
@@ -123,13 +125,13 @@ class ClassData():
 						beta  = C[4]
 						gamma = C[5]
 						exp.append(a+','+b+','+c+','+alpha+','+beta+','+gamma)
-						for x, y, z, r, f in zip(X, Y, Z, R, F):
+						for x, y, z, r, e in zip(X, Y, Z, R, E):
 							x = str(round(x, 5))
 							y = str(round(y, 5))
 							z = str(round(z, 5))
 							r = str(round(r, 5))
-							f = str(round(f, 5))
-							exp.append(x+','+y+','+z+','+r+','+f)
+							e = str(round(e, 5))
+							exp.append(x+','+y+','+z+','+r+','+e)
 						example = ','.join(exp)
 						TheFile.write(item.upper()+'.mtz'+','+label+',')
 						TheFile.write(example + '\n')
@@ -137,9 +139,103 @@ class ClassData():
 						os.remove(Mfilename)
 						os.remove(Pfilename)
 					except Exception as e:
-						print('\u001b[31m[-] {} failed compiling\u001b[0m'\
+						print('\u001b[31m[-] {} Failed: not compiling\u001b[0m'\
 						.format(item.upper()))
 						continue
+
+def Vectorise_Class(filename='DeepClass.csv', fp=np.float16, ip=np.int16):
+	'''
+	Since the .csv file cannot be loaded into RAM even that of a supercomputer,
+	this function vectorises the dataset normalises it as well as construct the
+	final tensors and export the result as a serial.
+	'''
+	# 1. Find number of rows
+	rows = len(open(filename).readlines()) - 1
+	# 2. Generate a list of random number of rows
+	lines = list(range(1, rows + 1))
+	random.shuffle(lines)
+	# 3. Open CSV file
+	File = open(filename)
+	# 4. Import a single row
+	all_lines_variable = File.readlines()
+	L, S, UCe, UCa, X, Y, Z, R, E = [], [], [], [], [], [], [], [], []
+	for i in lines:
+		# 7. Isolate labels and crystal data columns
+		line= all_lines_variable[i]
+		line= line.strip().split(',')
+		L.append(np.array(str(line[1]), dtype=str))
+		S.append(np.array(int(line[2]), dtype=ip))
+		UCe.append(np.array([float(i) for i in line[3:6]], dtype=fp))
+		UCa.append(np.array([float(i) for i in line[6:9]], dtype=fp))
+		# 6. Isolate points data columns
+		Pts = line[9:]
+		Pts = [float(i) for i in Pts]
+		# 7. Isolate different points data
+		X.append(np.array(Pts[0::5], dtype=fp))
+		Y.append(np.array(Pts[1::5], dtype=fp))
+		Z.append(np.array(Pts[2::5], dtype=fp))
+		R.append(np.array(Pts[3::5], dtype=fp))
+		E.append(np.array(Pts[4::5], dtype=fp))
+	# 8. Construct matrices
+	L  = np.array(L)
+	S  = np.array(S)
+	UCe= np.array(UCe)
+	UCa= np.array(UCa)
+	X  = np.array(X)
+	Y  = np.array(Y)
+	Z  = np.array(Z)
+	R  = np.array(R)
+	E  = np.array(E)
+	# 9. One-Hot encoding and normalisation
+	''' Y labels '''
+	label_encoder = LabelEncoder()
+	integer_encoded = label_encoder.fit_transform(L)
+	onehot_encoder = OneHotEncoder(sparse=False)
+	integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+	y = onehot_encoder.fit_transform(integer_encoded)
+	y = np.float16(y)
+	''' X features '''
+	categories = [sorted([x for x in range(1, 230+1)])]
+	S = S.reshape(-1, 1)
+	onehot_encoder = OneHotEncoder(sparse=False, categories=categories)
+	S = onehot_encoder.fit_transform(S) # One-hot encode S   [Space Groups]
+	mini = np.amin(UCe)
+	maxi = np.amax(UCe)
+	UCe = (UCe-mini)/(maxi-mini)     # Normalise min/max UCe [Unit Cell Edges]
+	mini = 90.0
+	maxi = 180.0
+	UCa = (UCa-mini)/(maxi-mini)     # Normalise min/max UCa [Unit Cell Angles]
+	mini = -1
+	maxi = 1
+	X = (X-mini)/(maxi-mini)         # Normalise min/max X   [X Coordinates]
+	mini = -1
+	maxi = 1
+	Y = (Y-mini)/(maxi-mini)         # Normalise min/max Y   [Y Coordinates]
+	mini = -1
+	maxi = 1
+	Z = (Z-mini)/(maxi-mini)         # Normalise min/max Z   [Z Coordinates]
+	mini = 2.5
+	maxi = 10
+	R = (R-mini)/(maxi-mini)         # Normalise min/max R   [Resolution]
+	mini = 0
+	maxi = np.amax(E)
+	E = (E-mini)/(maxi-mini)         # Normalise min/max E   [E-value]
+	# 10. Construct tensors - final features
+	Space = S
+	UnitC = np.concatenate([UCe, UCa], axis=1)
+	Coord = np.array([X, Y, Z, R, E])
+	Coord = np.swapaxes(Coord, 0, 2)
+	Coord = np.swapaxes(Coord, 0, 1)
+	S, UCe, UCa, X, Y, Z, R, E = [], [], [], [], [], [], [], []
+	# 11. Serialise tensors
+	with h5py.File('Y.hdf5', 'w') as Yh:
+		dset = Yh.create_dataset('default', data=y)
+	with h5py.File('Space.hdf5', 'w') as Sh:
+		dset = Sh.create_dataset('default', data=Space)
+	with h5py.File('UnitC.hdf5', 'w') as Uh:
+		dset = Uh.create_dataset('default', data=UnitC)
+	with h5py.File('Coord.hdf5', 'w') as Ch:
+		dset = Ch.create_dataset('default', data=Coord)
 
 class PhaseData():
 	''' Build a dataset for phase calculation from x-ray diffractions '''
@@ -152,6 +248,7 @@ class PhaseData():
 		Extracts required features from .mtz files. More info can be found here:
 		https://cci.lbl.gov/cctbx_docs/cctbx/cctbx.miller.html
 		'''
+		from iotbx.reflection_file_reader import any_reflection_file
 		hkl_file = any_reflection_file(filename)
 		arrays = hkl_file.as_miller_arrays(merge_equivalents=False)
 		for a in arrays:
@@ -184,10 +281,13 @@ class PhaseData():
 		for a in arrays:
 			label = str(a.info()).split(':')[-1].split(',')[-1]
 			if label == 'FOM':
-				# F-obs
-				F = list(a.expand_to_p1().f_sq_as_f().data())
-		nX, nY, nZ, nR, nF, nP = [], [], [], [], [], []
-		for x, y, z, r, f, p in zip(X, Y, Z, R, F, P):
+				# E-values in P1 space group
+				a.setup_binner(auto_binning=True)
+				a.binner()
+				e_val = a.quasi_normalize_structure_factors()
+				E = list(e_val.expand_to_p1().f_sq_as_f().data())
+		nX, nY, nZ, nR, nE, nP = [], [], [], [], [], []
+		for x, y, z, r, e, p in zip(X, Y, Z, R, E, P):
 			if r >= 10.0 or r <= 2.5:
 				continue
 			else:
@@ -195,16 +295,12 @@ class PhaseData():
 				nY.append(y)
 				nZ.append(z)
 				nR.append(r)
-				nF.append(f)
+				nE.append(e)
 				nP.append(p)
-		X = nX
-		Y = nY
-		Z = nZ
-		R = nR
-		F = nF
-		P = nP
-		return(S, C, X, Y, Z, R, F, P)
+		X, Y, Z, R, E, P = nX, nY, nZ, nR, nE, nP
+		return(S, C, X, Y, Z, R, E, P)
 	def run(self, IDs='IDs.txt'):
+		import tqdm
 		with open('temp', 'w') as temp:
 			size = []
 			with open(IDs) as f:
@@ -213,11 +309,10 @@ class PhaseData():
 					try:
 						self.download(item)
 					except:
-						print('\u001b[31m[-] {} failed to download\u001b[0m'\
-						.format(item.upper()))
+						print('\u001b[31m[-] {} Failed: could not download\u001b[0m'.format(item.upper()))
 						continue
 					try:
-						filename = item + '.mtz'
+						filename = './Phase/' + item + '.mtz'
 						S, C, X, Y, Z, R, F, P = self.features(filename)
 						assert len(X) == len(Y) == len(Z) == len(R) == len(F) == len(P),\
 						'\u001b[31m[-] {} Failed: values not equal\u001b[0m'.format(item.upper())
@@ -243,12 +338,11 @@ class PhaseData():
 						size.append(len(X))
 						os.remove(filename)
 					except:
-						print('\u001b[31m[-] {} failed compiling\u001b[0m'\
-						.format(item.upper()))
+						print('\u001b[31m[-] {} Failed: problem compiling\u001b[0m'.format(item.upper()))
 						continue
 			header = ['PDB_ID,Space_Group,Unit-Cell_a,Unit-Cell_b,Unit-Cell_c,Unit-Cell_Alpha,Unit-Cell_Beta,Unit-Cell_Gamma']
 			for i in range(1, max(size)+1):
-				header.append(',X_{},Y_{},Z_{},Resolution_{},F-obs_{},Phase{}'\
+				header.append(',X_{},Y_{},Z_{},Resolution_{},E-value_{},Phase_{}'\
 				.format(i, i, i, i, i, i))
 			header = ''.join(header)
 		with open('DeepPhase.csv', 'w') as f:
@@ -257,113 +351,9 @@ class PhaseData():
 				for line in t: f.write(line)
 		os.remove('temp')
 
-def Vectorise_Class(filename='DeepClass.csv', fp=np.float64, ip=np.int64):
-	'''
-	Since the .csv file cannot be loaded into RAM even that of a supercomputer,
-	this function vectorises the dataset normalises it as well as construct the
-	final tensors and export the result as a serial.
-	'''
-	# 1. Find number of rows
-	rows = len(open(filename).readlines()) - 1
-	# 2. Generate a list of random number of rows
-	lines = list(range(1, rows + 1))
-	random.shuffle(lines)
-	# 3. Open CSV file
-	File = open(filename)
-	# 4. Import a single row
-	all_lines_variable = File.readlines()
-	L, S, UCe, UCa, X, Y, Z, R, F = [], [], [], [], [], [], [], [], []
-	for i in lines:
-		# 5. Isolate labels and crystal data columns
-		line = all_lines_variable[i]
-		line = line.strip().split(',')
-		L.append(np.array(str(line[1]), dtype=str))
-		S.append(np.array(int(line[2]), dtype=ip))
-		UCe.append(np.array([float(i) for i in line[3:6]], dtype=fp))
-		UCa.append(np.array([float(i) for i in line[6:9]], dtype=fp))
-		# 6. Isolate points data columns
-		Pts = line[9:]
-		Pts = [float(i) for i in Pts]
-		# 7. Fill gaps with zeros
-		dif = 50000 - len(Pts)
-		Pts.extend([0.0]*dif)
-		# 8. Isolate different points data
-		X.append(np.array(Pts[0::5], dtype=fp))
-		Y.append(np.array(Pts[1::5], dtype=fp))
-		Z.append(np.array(Pts[2::5], dtype=fp))
-		R.append(np.array(Pts[3::5], dtype=fp))
-		F.append(np.array(Pts[4::5], dtype=fp))
-	# 9. Construct matrices
-	L  = np.array(L)
-	S  = np.array(S)
-	UCe= np.array(UCe)
-	UCa= np.array(UCa)
-	X  = np.array(X)
-	Y  = np.array(Y)
-	Z  = np.array(Z)
-	R  = np.array(R)
-	F  = np.array(F)
-	# 10. One-Hot encoding and normalisation
-	''' Y labels '''
-	label_encoder = LabelEncoder()
-	integer_encoded = label_encoder.fit_transform(L)
-	onehot_encoder = OneHotEncoder(sparse=False)
-	integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
-	y = onehot_encoder.fit_transform(integer_encoded)
-	y = np.float16(y)
-	''' X features '''
-	categories = [sorted([x for x in range(1, 230+1)])]
-	S = S.reshape(-1, 1)
-	onehot_encoder = OneHotEncoder(sparse=False, categories=categories)
-	S = onehot_encoder.fit_transform(S) # One-hot encode S   [Space Groups]
-	mini = np.amin(UCe)
-	maxi = np.amax(UCe)
-	UCe = (UCe-mini)/(maxi-mini)     # Normalise min/max UCe [Unit Cell Edges]
-	mini = 90.0
-	maxi = 180.0
-	UCa = (UCa-mini)/(maxi-mini)     # Normalise min/max UCa [Unit Cell Angles]
-	mini = -1
-	maxi = 1
-	X = (X-mini)/(maxi-mini)         # Normalise min/max X   [X Coordinates]
-	mini = -1
-	maxi = 1
-	Y = (Y-mini)/(maxi-mini)         # Normalise min/max Y   [Y Coordinates]
-	mini = -1
-	maxi = 1
-	Z = (Z-mini)/(maxi-mini)         # Normalise min/max Z   [Z Coordinates]
-	mini = 2.5
-	maxi = 10
-	R = (R-mini)/(maxi-mini)         # Normalise min/max R   [Resolution]
-	#mini = np.amin(F)
-	#maxi = np.amax(F)
-	#F = (F-mini)/(maxi-mini)        # Normalise min/max F   [F-Obs](Already Normalised)
-	# 11. Construct tensors - final features
-	Space = S
-	UnitC = np.concatenate([UCe, UCa], axis=1)
-	Coord = np.array([X, Y, Z, R, F])
-	Coord = np.swapaxes(Coord, 0, 2)
-	Coord = np.swapaxes(Coord, 0, 1)
-	S, UCe, UCa, X, Y, Z, R, F = [], [], [], [], [], [], [], []
-	# 12. Serialise tensors
-	with h5py.File('Y.hdf5', 'w') as Yh:
-		dset = Yh.create_dataset('default', data=y)
-	with h5py.File('Space.hdf5', 'w') as Sh:
-		dset = Sh.create_dataset('default', data=Space)
-	with h5py.File('UnitC.hdf5', 'w') as Uh:
-		dset = Uh.create_dataset('default', data=UnitC)
-	with h5py.File('Coord.hdf5', 'w') as Ch:
-		dset = Ch.create_dataset('default', data=Coord)
-	
-def main():
-	if sys.argv[1] == 'class':
-		Cls = ClassData()
-		Cls.run(IDs='Class.txt', max_size=10000)
-	elif sys.argv[1] == 'phase':
-		Phs = PhaseData()
-		Phs.run(IDs='Phase.txt')
-	elif sys.argv[1] == 'vectorise_class':
-		Vectorise_Class()
-	else:
-		print('\u001b[31m[-] Wrong argument\u001b[0m')
+Cls = ClassData()
+Cls.run(IDs='Class.txt', max_size=10)
+Vectorise_Class()
 
-if __name__ == '__main__': main()
+Phs = PhaseData()
+Phs.run(IDs='Class.txt')
