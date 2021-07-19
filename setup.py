07ddfc4ded8c114dfbc492e5ac7ce2afcce0f2fb
Sari Sabban <sari.sabban@gmail.com>
2021-07-19 10:24:11 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,36 +3,28 @@ import sys
 import subprocess
 import urllib.request
 
-"""
-# BASH COMMAND SEQUENCE
-sudo ln -s /usr/bin/python3 /usr/bin/python
-sudo apt install dssp libglu1-mesa-dev freeglut3-dev mesa-common-dev scons build-essential
-mkdir CCTBX
-cd ./CCTBX
-wget https://raw.githubusercontent.com/cctbx/cctbx_project/master/libtbx/auto_build/bootstrap.py
-python bootstrap.py --use-conda --python 38 --nproc=4
-bash ./Miniconda3-latest-Linux-x86_64.sh
-    yes
-    ./miniconda3
-    yes
-conda create -n Cenv
-conda activate Cenv
-conda install -c schrodinger pymol -y
-conda install -c conda-forge cctbx tqdm keras tensorflow -y
-conda install -c anaconda numpy scipy biopython pandas scikit-learn h5py h5py -y
-conda install -c open3d-admin open3d -y
-
-# IBEX SETUP
-conda create -n POINT
-conda activate POINT
-conda install -c anaconda numpy scipy scikit-learn h5py keras-gpu tensorflow-gpu -y
-conda deactivate
-"""
-
 def setup():
 	'''
 	Installs required dependencies for this script to work
 	https://github.com/cctbx/cctbx_project/
+	----------------------------------------------------------------------------
+	# BASH COMMAND SEQUENCE
+	sudo ln -s /usr/bin/python3 /usr/bin/python
+	sudo apt install dssp libglu1-mesa-dev freeglut3-dev mesa-common-dev scons build-essential
+	mkdir CCTBX
+	cd ./CCTBX
+	wget https://raw.githubusercontent.com/cctbx/cctbx_project/master/libtbx/auto_build/bootstrap.py
+	python bootstrap.py --use-conda --python 38 --nproc=4
+	bash ./Miniconda3-latest-Linux-x86_64.sh
+		yes
+		./miniconda3
+		yes
+	conda create -n Cenv
+	conda activate Cenv
+	conda install -c schrodinger pymol -y
+	conda install -c conda-forge cctbx tqdm keras tensorflow -y
+	conda install -c anaconda numpy scipy biopython pandas scikit-learn h5py -y
+	conda install -c open3d-admin open3d -y
 	'''
 	os.system('sudo ln -s /usr/bin/python3 /usr/bin/python')
 	os.system('sudo apt install dssp libglu1-mesa-dev freeglut3-dev mesa-common-dev scons build-essential')
@@ -44,7 +36,7 @@ def setup():
 	urllib.request.urlretrieve(url, 'bootstrap.py')
 	os.system('python bootstrap.py --use-conda --python {} --nproc=1'.format(V))
 	stdout = subprocess.Popen(
-		'bash ./Miniconda3-latest-Linux-x86_64.sh', #bash ./CCTBX/Miniconda3-latest-Linux-x86_64.sh
+		'bash ./Miniconda3-latest-Linux-x86_64.sh',
 		stdin=subprocess.PIPE,
 		stdout=subprocess.PIPE,
 		shell=True).communicate(b'\nyes\n./miniconda3\nyes\n')
@@ -59,4 +51,5 @@ def setup():
 	os.system('conda install -c schrodinger pymol -y')
 	os.system('conda install tqdm biopython h5py scipy pandas==1.0.5 scikit-learn==0.23.1 numpy==1.16.6 tensorflow==2.2.0 keras==2.3.1 -y')
 	os.system('conda install -c open3d-admin open3d -y')
-setup()  
+
+setup()
